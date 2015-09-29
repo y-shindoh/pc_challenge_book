406bff4bbf4cd6be4afd4979ef8b7424b9b20e47
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-09-29 19:20:28 +0900

    Add a new file for Section 2.3.

---
+++ 2.3/q_2.3.7.cpp
@@ -0,0 +1,27 @@
+/* -*- coding: utf-8; tab-width: 4 -*- */
+/**
+ * @file	q_2.3.7.cpp
+ * @brief	「プログラミングコンテストチャレンジブック」の§2.3 p.66の問題の回答
+ * @author	Yasutaka SHINDOH / 新堂 安孝
+ * @note	see http://www.amazon.co.jp/2-ebook/dp/B00CY9256C/ .
+ */
+
+/*
+  メモ:
+
+ */
+
+#include <cstddef>
+#include <cstdio>
+
+int
+main()
+{
+//	int n(4);
+//	int m(3);
+//	int M(1000);
+
+	std::printf("後回し。\n");
+
+	return 0;
+}
