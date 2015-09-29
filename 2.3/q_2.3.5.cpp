5600d92622540482ed63e9bf78b8dcf8939672ef
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-09-29 19:20:12 +0900

    Add a comment.

--- 2.3/q_2.3.5.cpp
+++ 2.3/q_2.3.5.cpp
@@ -1,7 +1,7 @@
 /* -*- coding: utf-8; tab-width: 4 -*- */
 /**
  * @file	q_2.3.5.cpp
- * @brief	「プログラミングコンテストチャレンジブック」の§2.3の問題の回答
+ * @brief	「プログラミングコンテストチャレンジブック」の§2.3 p.62の問題の回答
  * @author	Yasutaka SHINDOH / 新堂 安孝
  * @note	see http://www.amazon.co.jp/2-ebook/dp/B00CY9256C/ .
  */
@@ -10,8 +10,12 @@
   メモ:
   合計値に対するその時点の整数を何回使ったかの表を作り、
   整数の種類を1つずつ増やしていき表を更新する。
-  最終的に指定した合計値があるかどうかを確認する。
-  (注意: 自力では計算量の爆発しやすい解法しか思いつかなかった)
+  最終的に指定した合計値が出現したかどうかを確認する。
+
+  備考:
+  自力では計算量の爆発しやすい解法しか思いつかなかったため、
+  書籍のヒント (DPテーブルの使い方) を少し読んで書籍とは違うコードを実装した。
+  計算量は O(nK) で、書籍と変わらない。
  */
 
 #include <cstddef>
@@ -28,7 +32,6 @@ main()
 	const int K(17);
 
 	std::vector<int> b;
-	int h, k;
 
 	b.resize(K+1, INT_MAX);
 	b[0] = 0;
@@ -37,17 +40,17 @@ main()
 		if (0 < i) {
 			for (int j(0); j < K; ++j) {
 				if (b[j] == INT_MAX) continue;
-				b[j] = 0;
+				b[j] = 0;		// 整数の使用回数をリセット
 			}
 		}
 		for (int j(0); j < K; ++j) {
 			if (b[j] == INT_MAX) continue;
 			if (m[i] <= b[j]) continue;
-			k = j + a[i];	// 合計値
+			int k = j + a[i];	// 合計値
 			if (K < k) break;
-			h = b[j] + 1;	// 整数の使用回数
+			int h = b[j] + 1;	// 整数の使用回数
 			if (b[k] <= h) continue;
-			b[k] = h;
+			b[k] = h;			// 整数の使用回数を更新
 		}
 		if (b[K] != INT_MAX) break;
 	}
