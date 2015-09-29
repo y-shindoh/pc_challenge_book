82111c6c23f3986f2190695f42da6bab99ff61c2
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-09-29 19:19:59 +0900

    Add a comment.

--- 2.3/q_2.3.1.cpp
+++ 2.3/q_2.3.1.cpp
@@ -1,7 +1,7 @@
 /* -*- coding: utf-8; tab-width: 4 -*- */
 /**
  * @file	q_2.3.1.cpp
- * @brief	「プログラミングコンテストチャレンジブック」の§2.3の問題の回答
+ * @brief	「プログラミングコンテストチャレンジブック」の§2.3 p.52の問題の回答
  * @author	Yasutaka SHINDOH / 新堂 安孝
  * @note	see http://www.amazon.co.jp/2-ebook/dp/B00CY9256C/ .
  */
