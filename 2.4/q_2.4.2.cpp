a35bf49b710b2705eca5f7a26cb12d05fe525394
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-09-30 19:27:55 +0900

    Add a new file for Section 2.4.

---
+++ 2.4/q_2.4.2.cpp
@@ -0,0 +1,18 @@
+/* -*- coding: utf-8; tab-width: 4 -*- */
+/**
+ * @file	q_2.4.2.cpp
+ * @brief	「プログラミングコンテストチャレンジブック」の§2.4 p.75の問題の回答
+ * @author	Yasutaka SHINDOH / 新堂 安孝
+ * @note	see http://www.amazon.co.jp/2-ebook/dp/B00CY9256C/ .
+ */
+
+
+#include <cstdio>
+
+int
+main()
+{
+	std::printf("2.2/q_2.2.4.cppで回答済み。\n");
+
+	return 0;
+}
