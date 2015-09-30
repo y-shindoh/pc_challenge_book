000a60d419c2d76fdc07eab46a215a1386529f0d
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-09-30 19:27:43 +0900

    Add a new file for Section 2.4.

---
+++ 2.4/q_2.4.3.cpp
@@ -0,0 +1,83 @@
+/* -*- coding: utf-8; tab-width: 4 -*- */
+/**
+ * @file	q_2.4.3.cpp
+ * @brief	「プログラミングコンテストチャレンジブック」の§2.4 p.85の問題の回答
+ * @author	Yasutaka SHINDOH / 新堂 安孝
+ * @note	see http://www.amazon.co.jp/2-ebook/dp/B00CY9256C/ .
+ */
+
+/*
+  メモ:
+  N x 3の長さのunion find treeを用い、
+  各動物がA, B, Cそれぞれの場合について、同時に成立する情報をグルーピングする。
+  このunion find treeにより、後の情報の矛盾を検出する。
+
+  備考:
+  自力では解けなかったため、書籍の解説の冒頭を読んでから考えた。
+  (もっとも、それもすぐにはピンとこなかったのだが)
+ */
+
+#include <cstddef>
+#include <cstdio>
+#include <vector>
+#include "union_find_tree.hpp"
+
+int
+main()
+{
+	int N(100);
+	int K(7);
+	int d[][3] = {{1, 101, 1},
+				  {2, 1, 2},
+				  {2, 2, 3},
+				  {2, 3, 3},
+				  {1, 1, 3},
+				  {2, 3, 1},
+				  {1, 5, 5}};
+
+	std::vector<int> error;
+	ys::UnionFindTree<unsigned int> tree((N + 1) * 3);
+
+	for (int i(0); i < K; ++i) {
+		// 入力チェック
+		if ((1 != d[i][0] && 2 != d[i][0]) || (d[i][1] < 1 && N < d[i][1]) || (d[i][2] < 1 && N < d[i][2])) {
+			error.push_back(i);
+			continue;
+		}
+
+		if (d[i][0] == 1) {
+			// 情報1 (同じ種類)
+			if (tree.same(d[i][1], d[i][2] + N) || tree.same(d[i][1], d[i][2] + N * 2)) {
+				error.push_back(i);	// 異なる種類の関係が成立
+				continue;
+			}
+			else {
+				for (int j(0); j < 3; ++j) {
+					tree.unite(d[i][1] + N, d[i][2] + N);
+				}
+			}
+		}
+		else {
+			// 情報2 (食物連鎖)
+			if (tree.same(d[i][1], d[i][2]) || tree.same(d[i][1], d[i][2] + N * 2)) {
+				error.push_back(i);	// 連鎖しない関係が成立
+				continue;
+			}
+			else {
+				for (int j(0); j < 3; ++j) {
+					int k = (j + 1) % 3;
+					tree.unite(d[i][1] + N * j, d[i][2] + N * k);
+				}
+			}
+		}
+	}
+
+	std::printf("<<%lu>>\n", error.size());
+
+	for (auto it = error.begin(); it != error.end(); ++it) {
+		int k = *it;
+		std::printf("[%d] %d, %d, %d\n", k, d[k][0], d[k][1], d[k][2]);
+	}
+
+	return 0;
+}
---
+++ libraries/union_find_tree.hpp
@@ -0,0 +1,117 @@
+/* -*- coding: utf-8; tab-width: 4 -*- */
+/**
+ * @file	union_find_tree.hpp
+ * @brief	union find treeのテンプレート
+ * @author	Yasutaka SHINDOH / 新堂 安孝
+ * @note	http://www.slideshare.net/iwiwi/ss-3578491 と
+			http://algoogle.hadrori.jp/algorithm/union-find.html を参考に実装した。
+ */
+
+#ifndef	__UNION_FIND_TREE_HPP__
+#define	__UNION_FIND_TREE_HPP__ "union_find_tree.hpp"
+
+#include <cstddef>
+#include <cassert>
+#include <vector>
+
+namespace ys
+{
+	/**
+	 * @class	union find treeのテンプレート
+	 * @note	テンプレートの型 @a TYPE には符号なし整数を指定すること。
+	 */
+	template<typename TYPE>
+	class UnionFindTree
+	{
+	private:
+
+		std::vector<TYPE> parent_;	///< 代表のインデックス
+		std::vector<TYPE> rank_;	///< ランク
+
+		/**
+		 * グループ代表のインデックスの取得
+		 * @param[in]	i	インデックス
+		 * @return	グループ代表のインデックス
+		 */
+		size_t
+		find(size_t i)
+			{
+				assert(i < parent_.size());
+
+				size_t j = (size_t)parent_[i];
+
+				if (i != j) {
+					j = find(j);
+					parent_[i] = (TYPE)j;
+				}
+
+				return j;
+			}
+
+	public:
+
+		/**
+		 * コンストラクタ
+		 */
+		UnionFindTree(size_t n)
+			{
+				parent_.reserve(n);
+				rank_.resize(n, (TYPE)0);
+
+				for (size_t i(0); i < n; ++i) {
+					parent_.push_back((TYPE)i);
+				}
+			}
+
+		/**
+		 * デストラクタ
+		 */
+		~UnionFindTree()
+			{
+				;
+			}
+
+		/**
+		 * 2つのグループを統合
+		 * @param[in]	i	インデックスその1
+		 * @param[in]	j	インデックスその2
+		 */
+		void
+		unite(size_t i,
+			  size_t j)
+			{
+				assert(i < parent_.size());
+				assert(j < parent_.size());
+
+				i = find(i);
+				j = find(j);
+				if (i == j) return;
+
+				if (rank_[i] < rank_[j]) {
+					parent_[i] = (TYPE)j;
+				}
+				else {
+					parent_[j] = (TYPE)i;
+					if (rank_[i] == rank_[j]) rank_[i]++;
+				}
+			}
+
+		/**
+		 * 2つのインデックスが同一グループに所属しているか確認
+		 * @param[in]	i	インデックスその1
+		 * @param[in]	j	インデックスその2
+		 * @return	true: 所属している, false: 所属していない
+		 */
+		bool
+		same(size_t i,
+			 size_t j)
+			{
+				assert(i < parent_.size());
+				assert(j < parent_.size());
+
+				return find(i) == find(j);
+			}
+	};
+};
+
+#endif	// __UNION_FIND_TREE_HPP__
