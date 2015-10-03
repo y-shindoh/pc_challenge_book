878f8406179cfafe036f9425fe100b057b4d58b0
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-10-03 18:27:00 +0900

    Add a new file for Section 2.6.

---
+++ 2.6/q_2.6.2.cpp
@@ -0,0 +1,22 @@
+/* -*- coding: utf-8; tab-width: 4 -*- */
+/**
+ * @file	q_2.6.2.cpp
+ * @brief	「プログラミングコンテストチャレンジブック」の§2.6 p.108の問題の回答
+ * @author	Yasutaka SHINDOH / 新堂 安孝
+ * @note	see http://www.amazon.co.jp/2-ebook/dp/B00CY9256C/ .
+ */
+
+/*
+  メモ:
+
+ */
+
+#include <cstdio>
+
+int
+main()
+{
+	std::printf("後回し。\n");
+
+	return 0;
+}
