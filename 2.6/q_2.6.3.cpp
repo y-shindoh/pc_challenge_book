3230511fc8db88412d16f89ad945dbe7bf8c9ba6
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-10-03 19:29:49 +0900

    Add a new file for Section 2.6.

---
+++ 2.6/q_2.6.3.cpp
@@ -0,0 +1,45 @@
+/* -*- coding: utf-8; tab-width: 4 -*- */
+/**
+ * @file	q_2.6.3.cpp
+ * @brief	「プログラミングコンテストチャレンジブック」の§2.6 p.110の問題の回答
+ * @author	Yasutaka SHINDOH / 新堂 安孝
+ * @note	see http://www.amazon.co.jp/2-ebook/dp/B00CY9256C/ .
+ */
+
+/*
+  メモ:
+  特に無し。
+ */
+
+#include <cstdio>
+
+int
+main()
+{
+//	int n(53);
+	int n(295927);
+
+	if (0 < n) {	// 自然数を仮定
+		int r(1);
+
+		if (0 < n % 2) {
+			for (int i(3); i * i <= n; i += 2) {
+				if (0 < n % i) continue;
+				r = i;
+				break;
+			}
+		}
+		else {
+			r = 2;
+		}
+
+		if (r == 1) {
+			std::printf("YES\n");
+		}
+		else {
+			std::printf("NO (%d = %d x %d)\n", n, r, n / r);
+		}
+	}
+
+	return 0;
+}
