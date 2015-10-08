d485d56b9d6779fb031d1ea8df6cb45252548380
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-10-08 19:07:25 +0900

    Add a new file for Section 3.2.

---
+++ 3.2/q_3.2.8.cpp
@@ -0,0 +1,24 @@
+/* -*- coding: utf-8; tab-width: 4 -*- */
+/**
+ * @file	q_3.2.8.cpp
+ * @brief	「プログラミングコンテストチャレンジブック」の§3.2 p.150の問題の回答
+ * @author	Yasutaka SHINDOH / 新堂 安孝
+ * @note	see http://www.amazon.co.jp/2-ebook/dp/B00CY9256C/ .
+ */
+
+/*
+  メモ:
+  盤を圧縮する。
+  縦方向と横方向それぞれで、全く同じ状態が続く部分を1つにまとめ、
+  その後、隣接する白のマスを塗りつぶす再帰処理を用いつつ、白の塊を数える。
+ */
+
+#include <cstdio>
+
+int
+main()
+{
+	std::printf("解法は分かったが、出題内容に違和感があるのでスキップ。\n");
+
+	return 0;
+}
