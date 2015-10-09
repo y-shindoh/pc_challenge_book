c71f74d67532bc2c5b0f1d8cd15af31173ee897c
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-10-09 19:06:16 +0900

    Add a new file for Section 3.3.

---
+++ 3.3/q_3.3.3.cpp
@@ -0,0 +1,22 @@
+/* -*- coding: utf-8; tab-width: 4 -*- */
+/**
+ * @file	q_3.3.3.cpp
+ * @brief	「プログラミングコンテストチャレンジブック」の§3.2 p.163の問題の回答
+ * @author	Yasutaka SHINDOH / 新堂 安孝
+ * @note	see http://www.amazon.co.jp/2-ebook/dp/B00CY9256C/ .
+ */
+
+/*
+  メモ:
+
+ */
+
+#include <cstdio>
+
+int
+main()
+{
+	std::printf("後回し。\n");
+
+	return 0;
+}
