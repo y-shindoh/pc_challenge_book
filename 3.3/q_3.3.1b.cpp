36aae930615c89c2bd528ebd3e579e1cf1fffcba
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-10-13 21:17:01 +0900

    Fix wrong comments.

--- 3.3/q_3.3.1b.cpp
+++ 3.3/q_3.3.1b.cpp
@@ -1,7 +1,7 @@
 /* -*- coding: utf-8; tab-width: 4 -*- */
 /**
  * @file	q_3.3.1b.cpp
- * @brief	「プログラミングコンテストチャレンジブック」の§3.2 p.156の問題の回答
+ * @brief	「プログラミングコンテストチャレンジブック」の§3.3 p.156の問題の回答
  * @author	Yasutaka SHINDOH / 新堂 安孝
  * @note	see http://www.amazon.co.jp/2-ebook/dp/B00CY9256C/ .
  */
--- 3.3/q_3.3.4.cpp
+++ 3.3/q_3.3.4.cpp
@@ -1,7 +1,7 @@
 /* -*- coding: utf-8; tab-width: 4 -*- */
 /**
  * @file	q_3.3.4.cpp
- * @brief	「プログラミングコンテストチャレンジブック」の§3.2 p.168の問題の回答
+ * @brief	「プログラミングコンテストチャレンジブック」の§3.3 p.168の問題の回答
  * @author	Yasutaka SHINDOH / 新堂 安孝
  * @note	see http://www.amazon.co.jp/2-ebook/dp/B00CY9256C/ .
  */
