4f870289408fd1b08918ea2214de3eaa39aeaaa7
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-10-17 13:00:12 +0900

    Add comments.

--- 3.5/q_3.5.1a.cpp
+++ 3.5/q_3.5.1a.cpp
@@ -15,68 +15,111 @@
 #include <cstring>
 #include <climits>
 #include <vector>
+#include <tuple>
 #include <algorithm>
 
-#define	V	5
+typedef	std::tuple<int, int, int>	Edge;	// 行き先の頂点, 空き容量, 逆辺のインデックス
 
-struct edge { int to, cap, rev; };	// 行き先, 容量, 逆辺のインデックス
-
-std::vector<edge> G[V];	// グラフの隣接リスト
-bool used[V];			// DFSで既に調べたかのフラグ
-
-void add_edge(int from, int to, int cap)
+/**
+ * グラフに辺を追加 (同時に逆辺も追加)
+ * @param[in,out]	graph	グラフ
+ * @param[in]	from	辺の始点
+ * @param[in]	to	辺の終点
+ * @param[in]	capacity	辺の容量
+ */
+void
+AddEdge(std::vector< std::vector<Edge> >& graph,
+		int from,
+		int to,
+		int capacity)
 {
-	G[from].push_back((edge){to, cap, (int)G[to].size()});
-	G[to].push_back((edge){from, 0, (int)G[from].size() - 1});
+	graph[from].push_back(Edge(to, capacity, graph[to].size()));
+	graph[to].push_back(Edge(from, 0, graph[from].size() - 1));
 }
 
 /**
- * @param[in]	v	探索の始点
- * @param[in]	t	探索の終点
- * @param[in]	f	流量の最大値 (キャパシティを無視した値)
+ * 流量決定の1試行を実施
+ * @param[in,out]	graph	グラフ
+ * @param[in,out]	used	ノードの探索状況
+ * @param[in]	from	探索の始点
+ * @param[in]	to	探索の終点
+ * @param[in]	flow	流量の最大値 (キャパシティを無視した値)
  * @return	実際の流量
  */
-int dfs(int v, int t, int f = INT_MAX)
+int
+MaxFlowSubroutine(std::vector< std::vector<Edge> >& graph,
+				  bool* used,
+				  int from,
+				  int to,
+				  int flow = INT_MAX)
 {
-	if (v == t) return f;
-	used[v] = true;
-	for (int i = 0; i < G[v].size(); ++i) {
-		edge &e = G[v][i];
-		if (used[e.to]) continue;	// 探索済み
-		if (e.cap <= 0) continue;	// 空き容量なし
-		int d = dfs(e.to, t, std::min(f, e.cap));
-		if (d > 0) {
-			e.cap -= d;
-			G[e.to][e.rev].cap += d;
-			return d;
-		}
+	if (from == to) return flow;
+
+	used[from] = true;
+
+	for (int i = 0; i < graph[from].size(); ++i) {
+		int n = std::get<0>(graph[from][i]);	// 次の頂点
+		int& c = std::get<1>(graph[from][i]);	// 空き容量
+		int r = std::get<2>(graph[from][i]);	// 逆辺のインデックス
+
+		if (used[n]) continue;	// 探索済み => スキップ
+		if (c <= 0) continue;	// 空き容量なし => スキップ
+
+		// 探索 (深さ優先)
+		int d = MaxFlowSubroutine(graph, used, n, to, std::min(flow, c));
+		if (d <= 0) continue;	// 変化なし => 次の頂点
+		c -= d;
+		std::get<1>(graph[n][r]) += d;
+		return d;
 	}
+
 	return 0;
 }
 
-int max_flow(int s, int t)
+/**
+ * 流量の算出
+ * @param[in,out]	graph	グラフ
+ * @param[in]	from	探索の始点
+ * @param[in]	to	探索の終点
+ * @return	実際の流量
+ */
+int
+MaxFlow(std::vector< std::vector<Edge> >& graph,
+		int from,
+		int to)
 {
 	int flow = 0;
+	bool* used = new bool[graph.size()];
+
 	while ('-') {
-		std::memset((void*)used, 0, sizeof(bool) * V);
-		int f = dfs(s, t);
-		if (f == 0) return flow;
+		std::memset((void*)used, 0, sizeof(bool) * graph.size());
+		int f = MaxFlowSubroutine(graph, used, from, to);
+		if (f == 0) break;
 		flow += f;
 	}
+
+	delete [] used;
+
+	return flow;
 }
+
+#define	V	5
 
 int
 main()
 {
-	add_edge(0, 1, 10);
-	add_edge(0, 2, 2);
-	add_edge(1, 2, 6);
-	add_edge(1, 3, 6);
-	add_edge(2, 4, 5);
-	add_edge(3, 2, 3);
-	add_edge(3, 4, 8);
+	std::vector< std::vector<Edge> > graph;
 
-	std::printf("%d\n", max_flow(0, 4));
+	graph.resize(V);
+	AddEdge(graph, 0, 1, 10);
+	AddEdge(graph, 0, 2, 2);
+	AddEdge(graph, 1, 2, 6);
+	AddEdge(graph, 1, 3, 6);
+	AddEdge(graph, 2, 4, 5);
+	AddEdge(graph, 3, 2, 3);
+	AddEdge(graph, 3, 4, 8);
+
+	std::printf("%d\n", MaxFlow(graph, 0, 4));
 
 	return 0;
 }
