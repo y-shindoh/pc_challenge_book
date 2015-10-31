0c4041c48b78bd02f1dec7b14a60c63c18dcef75
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-10-31 13:26:57 +0900

    Modify a file for Section 2.5.

--- 2.5/q_2.5.2.cpp
+++ 2.5/q_2.5.2.cpp
@@ -8,14 +8,13 @@
 
 /*
   メモ:
-  最短経路問題の各手法において、
+  最短経路問題の各手法 (今回はダイクストラ法を用いた) において、
   各コストの保存時に上位2番目までのコストを保持すれば良い。
 
   備考:
   最初に考えたのは計算量の多い解だった。
  */
 
-#include <cstddef>
 #include <cstdio>
 #include <climits>
 #include <vector>
@@ -24,25 +23,31 @@
 
 typedef	std::pair<int, int>	CV;	// 累計コストと頂点のペア
 
-#define	V	4	// 頂点数
-#define	E	4	// 辺数
-#define	S	0	// スタートの頂点
+#define	N	4		// 頂点数
+#define	R	4		// 辺数 (無向グラフと考えて)
+#define	E	(R*2)	// 辺数 (有向グラフと考えて)
+#define	S	0		// スタートの頂点
+#define	G	(N-1)	// ゴールの頂点
 
 int
 main()
 {
 	// 辺の始点, 終点, コスト (0以上)
 	int edge[E][3] = {{0, 1, 100},	// 1->2: 100
+					  {1, 0, 100},	// 2->1: 100
 					  {1, 2, 250},	// 2->3: 250
 					  {1, 3, 200},	// 2->4: 200
-					  {2, 3, 100}};	// 3->4: 100
-	int d[V][2];	// 各頂点の累計コスト上位2件
+					  {2, 1, 250},	// 3->2: 250
+					  {2, 3, 100},	// 3->4: 100
+					  {3, 1, 200},	// 4->2: 200
+					  {3, 2, 100}};	// 4->3: 100
+	int d[N][2];	// 各頂点の累計コスト上位2件
 
 	int v, c;
 	CV cv;
 	std::priority_queue<CV, std::vector<CV>, std::greater<CV> > queue;
 
-	for (int i(0); i < V; ++i) d[i][0] = d[i][1] = INT_MAX;
+	for (int i(0); i < N; ++i) d[i][0] = d[i][1] = INT_MAX;
 	queue.push(CV(0, S));
 
 	while (0 < queue.size()) {
@@ -67,8 +72,8 @@ main()
 	}
 
 	for (int h(0); h < 2; ++h) {
-		std::printf("<<%d>>\n", h);
-		for (int i(0); i < V; ++i) {
+		std::printf("<<%d>>\n%d (", h, d[G][h]);
+		for (int i(0); i < N; ++i) {
 			if (0 < i) std::printf(", ");
 			if (d[i][h] != INT_MAX) {
 				std::printf("%d", d[i][h]);
@@ -77,7 +82,7 @@ main()
 				std::printf("-");
 			}
 		}
-		std::printf("\n");
+		std::printf(")\n");
 	}
 
 	return 0;
